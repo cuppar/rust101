f387a876c3836cea2b13656980ab05222a7e6f7e
cuppar <cuppar@163.com>
2023-12-11 13:15:06 +0800

    09

--- src/part09.rs
+++ src/part09.rs
@@ -3,7 +3,6 @@
 
 use part05::BigInt;
 
-
 pub struct Iter<'a> {
     num: &'a BigInt,
     idx: usize, // the index of the last number that was returned
@@ -18,10 +17,11 @@ impl<'a> Iterator for Iter<'a> {
         // First, check whether there's any more digits to return.
         if self.idx == 0 {
             // We already returned all the digits, nothing to do.
-            unimplemented!()
+            None
         } else {
             // Otherwise: Decrement, and return next digit.
-            unimplemented!()
+            self.idx -= 1;
+            Some(self.num.data[self.idx])
         }
     }
 }
@@ -29,14 +29,17 @@ impl<'a> Iterator for Iter<'a> {
 // All we need now is a function that creates such an iterator for a given `BigInt`.
 impl BigInt {
     fn iter(&self) -> Iter {
-        unimplemented!()
+        Iter {
+            num: self,
+            idx: self.data.len(),
+        }
     }
 }
 
 // We are finally ready to iterate! Remember to edit `main.rs` to run this function.
 pub fn main() {
     let b = BigInt::new(1 << 63) + BigInt::new(1 << 16) + BigInt::new(1 << 63);
-    for digit in b.iter() {
+    for digit in &b {
         println!("{}", digit);
     }
 }
@@ -59,9 +62,53 @@ fn print_digits_v2(b: &BigInt) {
 }
 
 // **Exercise 09.1**: Write a testcase for the iterator, making sure it yields the corrects numbers.
-// 
+//
+#[test]
+fn test_iter() {
+    let b = BigInt::from_vec(vec![1, 2, 3]);
+    let mut iter = b.iter();
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), None);
+}
 // **Exercise 09.2**: Write a function `iter_ldf` that iterates over the digits with the
 // least-significant digits coming first. Write a testcase for it.
+
+pub struct IterLdf<'a> {
+    num: &'a BigInt,
+    idx: usize,
+}
+
+impl<'a> Iterator for IterLdf<'a> {
+    type Item = u64;
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.idx == self.num.data.len() {
+            None
+        } else {
+            let idx = self.idx;
+            self.idx += 1;
+            Some(self.num.data[idx])
+        }
+    }
+}
+
+impl BigInt {
+    fn iter_ldf(&self) -> IterLdf {
+        IterLdf { num: self, idx: 0 }
+    }
+}
+
+#[test]
+fn test_iter_ldf() {
+    let b = BigInt::from_vec(vec![1, 2, 3]);
+    let mut iter = b.iter_ldf();
+    assert_eq!(iter.next(), Some(1));
+    assert_eq!(iter.next(), Some(2));
+    assert_eq!(iter.next(), Some(3));
+    assert_eq!(iter.next(), None);
+    assert_eq!(iter.next(), None);
+}
 
 // ## Iterator invalidation and lifetimes
 
@@ -69,7 +116,7 @@ fn iter_invalidation_demo() {
     let mut b = BigInt::new(1 << 63) + BigInt::new(1 << 16) + BigInt::new(1 << 63);
     for digit in b.iter() {
         println!("{}", digit);
-        /*b = b + BigInt::new(1);*/                                 /* BAD! */
+        /*b = b + BigInt::new(1);*/ /* BAD! */
     }
 }
 
@@ -83,4 +130,3 @@ impl<'a> IntoIterator for &'a BigInt {
     }
 }
 // With this in place, you can now replace `b.iter()` in `main` by `&b`. Go ahead and try it! <br/>
-
